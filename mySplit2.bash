69222f0d5667cea8de5bd5d83553d701ec671633
jkool702 <107448833+jkool702@users.noreply.github.com>
2023-06-12 09:27:41 -0400

    Update mySplit2.bash

--- mySplit2.bash
+++ mySplit2.bash
@@ -1,17 +1,26 @@
 #!/bin/bash        
 
-    
 mySplit2() {
-    local tDir kk
-    local -a A
+    local tDir kk inotifyPID
+    local -a A p_PID
 
     tDir=/tmp/"$(mktemp -d .mySplit2.XXXXXXXXX)" 
     mkdir -p "${tDir}"
     touch "${tDir}"/.record
     
     {
+    
+        trap 'kill ${inotifyPID}; exec {fd_inotify}>&-; [[ "${tDir}" == /tmp ]] || rm -r "${tDir}"' EXIT
+        exec {fd_inotify}<><(:)
+    
+        # background inotify process
+        {
+            inotifywait -m -e modify,close "${tDir}"/.record | busybox tr -cd $'\n' >&${fd_inotify}
+        } &
         
-        # background porocess - append stdin pipe to tmpfile. IMPORTANT: use '>>', not '>'
+        inotifyPID=$!
+    
+        # background write porocess - append stdin pipe to tmpfile. IMPORTANT: use '>>', not '>'
         {
             cat >>"${tDir}"/.record 
             
@@ -19,44 +28,35 @@ mySplit2() {
             touch "${tDir}"/.done
         } 0<&${fd_stdin} &
         
-        exec {fd_continue}<><(:)
-        exec {fd_inotify}<><(:)
-        trap 'exec {fd_continue}>&- {fd_inotify}>&-' EXIT
-        
-        # background inotify monitoring process 
-        {
-            inotifywait -m -e modify "${tDir}"/.record | busybox tr -d [^\n] | while read; do
-                read -t 0.01 -N 1 -u ${fd_inotify} && printf '1' >&${fd_continue}
-            done
-        } &
-   
-    
-        # main process - read tmpfile and truncate after every read
+        p_PID+=($!)
+
+        # main read process - read tmpfile and truncate after every read
         { 
-            until [[ -f  "${tDir}"/.done ]] && [[ -z $(<"${tDir}"/.record) ]]; do      
+            runFlag=true
+
+            while ${runFlag} && read -N 1 -u ${fd_inotify}; do 
                 
-                # mapfile record into array A 
-                mapfile -t A <"${tDir}"/.record 
-                
-                # if we read something truncate tmpfile and do something with the read data, otherwise wait
-                if [[ ${#A[@]} == 0 ]]; then
-                    [[ -f  "${tDir}"/.done ]] || {
-                        printf '1' >&${fd_inotify}
-                        read -N 1 -u ${fd_continue}
-                    }
-                else
-                    # truncate tmpfile
-                    printf '%s' "$(busybox tail -n +$(( ${#A[@]} + 1 )) <"${tDir}"/.record)" >"${tDir}"/.record
-                
-                    # loop through A and do _________
-                    # printf doesnt need a loop for this, but in general for more complex stuff you probably will
-                    for kk in ${!A[@]}; do
-                        printf '%s\n' "${A[$kk]}"
-                    done 
+                # if tmpfile changed then mapfile record into array A; else continue
+                if [[ -s "${tDir}"/.record ]]; then
+                        # read  tmpfile
+                        mapfile -t A <"${tDir}"/.record 
+                else 
+                    # end condition
+                    [[ -f "${tDir}"/.done ]] && runFlag=false
+                    continue
                 fi
 
+                # truncate tmpfile
+                printf '%s' "$(busybox tail -n +$(( ${#A[@]} + 1 )) <"${tDir}"/.record)" >"${tDir}"/.record
+                
+                # loop through A and do _________
+                # printf doesnt need a loop for this, but in general for more complex stuff you probably will
+                for kk in ${!A[@]}; do
+                    printf '%s\n' "${A[$kk]}"
+                done 
             done
-        }
+        } 
+
     } {fd_stdin}<&0
     
 } 
