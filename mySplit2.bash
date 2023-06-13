01d1de5e285c1a413a09db7290dd56bc031b3e93
jkool702 <107448833+jkool702@users.noreply.github.com>
2023-06-13 00:34:21 -0400

    Update mySplit2.bash

--- mySplit2.bash
+++ mySplit2.bash
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-mySplit2 ()
-{
+mySplit2() {
     # make vars local
-    local tDir kk ;
+    local tDir kk;
     local -a A;
     
     # setup tmpdir
@@ -17,12 +16,12 @@ mySplit2 ()
         exec {fd_inotify}<><(:);
         trap 'exec {fd_inotify}>&-;' EXIT;
         {
-            inotifywait -m -e modify "${tDir}"/.record | busybox tr -cd $'\n' >&${fd_inotify}
+            inotifywait -m -e modify "${tDir}"/.record | busybox tr -cd $'\n' >&${fd_inotify};
         } &
         
         # background process to append stdin pipe to tmpfile. IMPORTANT: use '>>', not '>'
         {
-            cat >>"${tDir}"/.record
+            cat >>"${tDir}"/.record;
             
             # indicate there is no more data to write
             touch "${tDir}"/.done;
@@ -34,27 +33,26 @@ mySplit2 ()
                 if [[ -s "${tDir}"/.record ]]; then
                     mapfile -t A < "${tDir}"/.record;
                 else
-                    [[ -f "${tDir}"/.done ]] && break
-                    read -u ${fd_inotify}
-                    continue
-                fi;
+                    [[ -f "${tDir}"/.done ]] && break;
+                    read -u ${fd_inotify};
+                    continue;
+                fi
                 
                 # debug - see how many lines each mapfile consumed
                 #printf 'READ %s LINES\n' "${#A[@]}" 1>&2;
  
                 # truncate tmpfile
-                printf '%s' "$(busybox tail -n +$(( ${#A[@]} + 1 )) < "${tDir}"/.record)" > "${tDir}"/.record;
+                printf '%s' "$(busybox tail -n +$(( ${#A[@]} + 1 )) <"${tDir}"/.record)" >"${tDir}"/.record;
 
                 # loop through array A and do _________
                 # printf doesnt need a loop for this, but in general for more complex stuff you probably will
-                for kk in ${!A[@]};
-                do
+                for kk in ${!A[@]}; do
                     printf '%s\n' "${A[$kk]}";
-                done;
+                done
             done
         }
     } {fd_stdin}<&0
     
     # remove tmpdir if the code finished normally
-    [[ "${tDir}" == /tmp ]] || rm -r "${tDir}"
+    [[ "${tDir}" == /tmp ]] || rm -r "${tDir}";
 }
