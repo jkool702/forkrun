dd549d364a249e7bc7f0bdb92060973c4256704f
jkool702 <107448833+jkool702@users.noreply.github.com>
2023-06-14 08:31:52 -0400

    Update mySplit2.bash

--- mySplit2.bash
+++ mySplit2.bash
@@ -1,58 +1,74 @@
 #!/bin/bash
 
-mySplit2() {
+mySplit2 ()
+{
     # make vars local
-    local tDir kk;
+    local tDir kk bbPath printfFastFlag
     local -a A;
+    
+    # check for busybox
+    bbPath="$(type -p busybox)"
+    
+    # choose whether to use loop or not when printing ${A[@]} with printf
+    #printfFastFlag=true
+    printfFastFlag=false
     
     # setup tmpdir
     tDir=/tmp/"$(mktemp -d .mySplit2.XXXXXX)";
     mkdir -p "${tDir}";
     touch "${tDir}"/.record;
-    
     {
         # setup inotify anonymous pipe and background process (in case data is arriving slowly on stdin)
         # this keeps the read process idle until there is actually something to read without using much CPU time
         exec {fd_inotify}<><(:);
         trap 'exec {fd_inotify}>&-;' EXIT;
         {
-            inotifywait -m -e modify "${tDir}"/.record | busybox tr -cd $'\n' >&${fd_inotify};
+            { printf '\n'; inotifywait -m -e modify,close "${tDir}"/.record; } | "${bbPath}" tr -cd $'\n' >&${fd_inotify}
         } &
         
         # background process to append stdin pipe to tmpfile. IMPORTANT: use '>>', not '>'
         {
-            cat >>"${tDir}"/.record;
+            cat >>"${tDir}"/.record
             
             # indicate there is no more data to write
             touch "${tDir}"/.done;
+            printf '\n' >&${fd_inotify}
         } <&${fd_stdin} &
         
         # main process - read tmpfile and truncate after every read
         {
             while true; do
                 if [[ -s "${tDir}"/.record ]]; then
-                    mapfile -t A < "${tDir}"/.record;
+                    # data is available - read it
+                    mapfile -t A <"${tDir}"/.record;
                 else
-                    [[ -f "${tDir}"/.done ]] && break;
-                    read -u ${fd_inotify};
-                    continue;
-                fi
+                    # check for end condition
+                    [[ -f "${tDir}"/.done ]] && break
+                    
+                    # blocking read of {fd_inotify}
+                    [[ -s "${tDir}"/.record ]] || read -u ${fd_inotify}
+                    continue
+                fi;
                 
                 # debug - see how many lines each mapfile consumed
                 #printf 'READ %s LINES\n' "${#A[@]}" 1>&2;
  
                 # truncate tmpfile
-                printf '%s' "$(busybox tail -n +$(( ${#A[@]} + 1 )) <"${tDir}"/.record)" >"${tDir}"/.record;
+                printf '%s' "$("${bbPath}" tail -n +$(( ${#A[@]} + 1 )) < "${tDir}"/.record)" > "${tDir}"/.record;
 
-                # loop through array A and do _________
+                # loop through A and do _________
                 # printf doesnt need a loop for this, but in general for more complex stuff you probably will
-                for kk in ${!A[@]}; do
-                    printf '%s\n' "${A[$kk]}";
-                done
+                if ${printfFastFlag}; then
+                    printf '%s\n' "${A[@]}"
+                else
+                    for kk in ${!A[@]}; do
+                        printf '%s\n' "${A[$kk]}";
+                    done;
+                fi
             done
         }
     } {fd_stdin}<&0
     
     # remove tmpdir if the code finished normally
-    [[ "${tDir}" == /tmp ]] || rm -r "${tDir}";
+    [[ "${tDir}" == /tmp ]] || rm -r "${tDir}"
 }
