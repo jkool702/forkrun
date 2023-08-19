0eadb122fec13b35130a29e638f60f84a8558733
jkool702 <107448833+jkool702@users.noreply.github.com>
2023-08-19 01:29:23 -0400

    several bugs fixed and has been further optimized

--- mySplit_nLinesAuto_test.bash
+++ mySplit_nLinesAuto_test.bash
@@ -6,7 +6,7 @@ mySplit() {
     # input 2: number of coprocs to use. Default is $(nproc)
             
     # make vars local
-    local tmpDir fPath nLinesUpdateCmd outStr exitTrapStr nOrder inotifyFlag initFlag nLinesAutoFlag nOrderFlag rmDirFlag pipeReadFlag
+    local tmpDir fPath nLinesUpdateCmd outStr exitTrapStr nOrder coprocSrcCode inotifyFlag initFlag nLinesAutoFlag nOrderFlag rmDirFlag pipeReadFlag
     local -i nLines nLinesCur nLinesMax nProcs nDone kk
     local -a A p_PID 
   
@@ -23,18 +23,18 @@ mySplit() {
         [[ "${2}" =~ ^[0-9]*[1-9]+[0-9]*$ ]] && nProcs="${2}" || nProcs=$({ type -a nproc 2>/dev/null 1>/dev/null && nproc; } || grep -cE '^processor.*: ' /proc/cpuinfo || printf '4')
             
         # if reading 1 line at as time (and not automatically adjusting it) skip saving the data in a tmpfile and read directly from stdin pipe
-        ${nLinesAutoFlag} || { [[ ${nLines} == 1 ]] && [[ -z ${pipeReadFlag} ]] && pipeReadFlag=true; }
+        ${nLinesAutoFlag} || { [[ ${nLines} == 1 ]] && : "${pipeReadFlag:=true}"; }
 
 
         # check for inotifywait
-        type -p inotifywait 2>/dev/null 1>/dev/null && inotifyFlag=true || inotifyFlag=false
+        type -p inotifywait 2>/dev/null 1>/dev/null && inotifyFlag=true
         
         # set defaults for control flags/parameters
-        : "${nOrderFlag:=false}" "${rmDirFlag:=true}" "${nLinesMax:=512}" "${pipeReadFlag:=false}"
+        : "${nOrderFlag:=false}" "${rmDirFlag:=true}" "${nLinesMax:=512}" "${pipeReadFlag:=false}" "${inotifyFlag:=false}"
             
-        ${pipeReadFlag} && ${nLinesAutoFlag} && printf '%s\n' '' 'WARNING: automatically adjusting number of lines used per function call not supoported when reading from a pipe' '         Disabling automatic adjustment of number of lines used per function call ' '' >&${fd_stderr} && nLinesAutoFlag=false
+        ${pipeReadFlag} && ${nLinesAutoFlag} && { printf '%s\n' '' 'WARNING: automatically adjusting number of lines used per function call not supoported when reading directly from stdin pipe' '         Disabling reading directly from stdin pipe...a tmpfile will be used' '' >&${fd_stderr}; pipeFLag=false; }
     
-        ${rmDirFlag} || printf '\ntmpDir: %s\n\n' "${tmpDir}" >&${fd_stderr}
+        ${rmDirFlag} || printf '\ntmpDir path: %s\n\n' "${tmpDir}" >&${fd_stderr}
         
         # spawn a coproc to write stdin to a tmpfile
         # After we are done reading all of stdin print total line count to a file 
@@ -54,11 +54,11 @@ mySplit() {
         if ${inotifyFlag}; then
         
             # add 1 newline for each coproc to fd_inotify
-            source <(printf 'printf '"'"'%%.0s\\n'"'"' {0..%s} ' $(( $nProcs-1 ))) >&${fd_inotify}
+            source <(printf 'printf '"'"'%%.0s\\n'"'"' {1..%s} ' ${nProcs} >&${fd_inotify})
             
             #{ coproc pNotify {
            
-            inotifywait -m -e modify,close --format '' "${fPath}" 2>/dev/null >&${fd_inotify} &
+            inotifywait -q -m -e modify,close --format '' "${fPath}" >&${fd_inotify} &
             
             #   }
             #}
@@ -82,35 +82,30 @@ mySplit() {
             nDone=0
             
             { coproc pAuto {
-                    nLinesUpdateCmd="$(printf "echo "; source <(echo 'echo '"'"'$(( 1 + ( $(wc -l <"'"'"'"${fPath}"'"'"'") - $(<"'"'"'"${tmpDir}"'"'"'"/.n0'"'"' '"'"') - $(<"'"'"'"${tmpDir}"'"'"'"/.n'"'"'{1..'"$(( ${nProcs} - 1 ))"}' '"'"') ) / '"${nProcs}"' ))'"'"))"
-                    source <(cat<<EOF
+                    trap - EXIT
+                    #nLinesUpdateCmd="$(printf "echo "; source <(echo 'echo '"'"'$(( 1 + ( $(wc -l <"'"'"'"${fPath}"'"'"'") - $(<"'"'"'"${tmpDir}"'"'"'"/.n0'"'"' '"'"') - $(<"'"'"'"${tmpDir}"'"'"'"/.n'"'"'{1..'"$(( ${nProcs} - 1 ))"}' '"'"') ) / '"${nProcs}"' ))'"'"))"
+#source <(cat<<EOF
 nLinesUpdate() {
-    local nLinesNew
-    nLinesNew=\$(${nLinesUpdateCmd})
-    (( \${nLinesNew} > ${nLinesMax} )) && nLinesNew=${nLinesMax}
-    (( \${nLinesNew} > \$(<"${tmpDir}"/.nLines) )) && echo \${nLinesNew} >"${tmpDir}"/.nLines && printf 'Changing nLines to %s\\n' "\${nLinesNew}" >&${fd_stderr} 
-    [[ \${nLinesNew} == ${nLinesMax} ]] && return 0 || return 1
+    local -i nLinesNew rVal
+    rVal=0
+    [[ -f "${tmpDir}"/.done ]] && ((rVal++))
+    nLinesNew=$(( 1 + ( $(wc -l <"${fPath}") / ${nProcs} ) ))
+    (( ${nLinesNew} >= ${nLinesMax} )) && { nLinesNew=${nLinesMax}; ((rVal++)); }
+    (( ${nLinesNew} > $(<"${tmpDir}"/.nLines) )) && printf '%s\n' ${nLinesNew} >"${tmpDir}"/.nLines && printf 'Changing nLines to %s\n' "${nLinesNew}" >&${fd_stderr} 
+    return ${rVal}
 }
-EOF
-                    )
+#EOF
+#)
             
                     while read -u ${fd_nLinesAuto}; do
                         [[ ${REPLY} == 0 ]] && break
                         nLinesUpdate || break
-                        [[ -f "${tmpDir}"/.done ]] && break
                     done
                 } 
-            }
+            } 2>/dev/null
         else
             nLinesCur=${nLines}
         fi
-
-        # populate {fd_+continue} with an initial '1' 
-        # {fd_continue} will act as an exclusive read lock - when there is a '1' buffered in the pipe then nothinghas an read lock: 
-        # a process reads 1 byte from {fd_continue} to get the read lock, and that process writes a '1' back to the pipe to release the read lock
-    
-        # dont exit read loop during init 
-        initFlag=true
         
         # setup (ordered) output
         if ${nOrderFlag}; then
@@ -118,14 +113,12 @@ EOF
             mkdir -p "${tmpDir}"/.out
             outStr='>"'"${tmpDir}"'"/.out/x${nOrder}'
                         
-            coproc pOrder ( 
+            { coproc pOrder ( 
+                trap - EXIT
                 
-                local v0 v9
+                local -i v0 v9
                 
-                v9='' 
-                v0=''
-                
-                printf '%s\n' {00..09} {10..89} >&${fd_nOrder}
+                printf '%s\n' {00..89} >&${fd_nOrder}
                 
                 while true; do
                     v9="${v9}9"
@@ -134,14 +127,19 @@ EOF
                     source <(printf '%s\n' 'printf '"'"'%s\n'"'"' {'"${v9}"'00'"${v0}"'..'"${v9}"'89'"${v9}"'} >&'"${fd_nOrder}")
                 done
             )
-
+            } 2>/dev/null
         else 
             
             outStr='>&'"${fd_stdout}"; 
         fi
 
-        # initialize fd_continue    
+        # populate {fd_continue} with an initial '1' 
+        # {fd_continue} will act as an exclusive read lock - when there is a '1' buffered in the pipe then nothinghas an read lock: 
+        # a process reads 1 byte from {fd_continue} to get the read lock, and that process writes a '1' back to the pipe to release the read lock
         printf '\n' >&${fd_continue}; 
+
+        # dont exit read loop during init 
+        initFlag=true
         
         # spawn $nProcs coprocs
         # on each loop, they will read {fd_continue}, which blocks them until they have exclusive read access
@@ -152,9 +150,9 @@ EOF
         # NOTE: by putting the read fd in a brace group surrounding all the coprocs, they will all use the same file descriptor
         # this means that whenever a coproc reads data it will start reading at the point the last coproc stopped reading at
         # This basically means that all you need to do is make sure 2 processes dont read at the same time.
-        for kk in $( source <(printf '%s\n' 'printf '"'"'%s '"'"' {0..'"$(( ${nProcs} - 1 ))"'}') ); do
-source <(cat<<EOF0
-coproc p${kk} {
+        coprocSrcCode="$(cat<<EOF0
+{ coproc p%s {
+trap - EXIT
 while true; do
     read -u ${fd_continue} 
 $(${nLinesAutoFlag} && cat<<EOF1
@@ -166,40 +164,51 @@ $(${nOrderFlag} && cat<<EOF2
     read -u ${fd_nOrder} nOrder
 EOF2
 )
-    printf '\\n' >&${fd_continue}; 
+    printf '\\\\n' >&${fd_continue}; 
     [[ \${#A[@]} == 0 ]] && { 
         
         if [[ -f "${tmpDir}"/.done ]]; then
-            if \${initFlag}; then
-                initFlag=false
-            else
-                printf '\\n' >&${fd_inotify}
-                break
-            fi
+            [[ -f "${tmpDir}"/.quit ]] && break
 $(${inotifyFlag} && cat<<EOF3
+                printf '%%.0s\\\\n' {0..${nProcs}} >&${fd_inotify}
+EOF3
+)
+            \${initFlag} && initFlag=false || { touch "${tmpDir}"/.quit; break; }
+$(${inotifyFlag} && cat<<EOF4
         else        
             read -u ${fd_inotify}
-EOF3
+EOF4
 )
         fi
         continue
     }
     
-    printf '%s\\n' "\${A[@]}" ${outStr}
-    sed -i "1,\${nLinesCur}d" "${fPath}"
+    printf '%%s\\\\n' "\${A[@]}" ${outStr}
+    sed -i "1,\${#A[@]}d" "${fPath}"
 
+$(${nLinesAutoFlag} && cat<<EOF5
     \${nLinesAutoFlag} && { 
         [[ \${nLinesCur} == ${nLinesMax} ]] && { nLinesAutoFlag=false; printf '0\\n' >&${fd_nLinesAuto}; } || {
             nDone+=\${#A[@]}
-            echo \${nDone} >"${tmpDir}"/.n${kk}
-            printf '\\n' >&${fd_nLinesAuto}
+            printf '%%s\\\\n' \${nDone} >"${tmpDir}"/.n%s
+            printf '\\\\n' >&${fd_nLinesAuto}
         }
     }
-done
-}
-p_PID+=(\${p${kk}_PID})
-EOF0
+EOF5
 )
+done
+} 2>&${fd_stderr}
+} 2>/dev/null
+p_PID+=(\${p%s_PID})
+EOF0
+)"
+
+        #coprocSrcCode_kk=$(echo "${coprocSrcCode}" | grep -oE '[^%]%s' | wc -l)
+        
+        for kk in $( source <(printf '%s\n' 'printf '"'"'%s '"'"' {0..'"$(( ${nProcs} - 1 ))"'}') ); do
+            [[ -f "${tmpDir}"/.quit ]] && break
+            #source <(printf "${coprocSrcCode}" $(source <(printf 'printf "${kk}%%.0s " {1..'"${coprocSrcCode_kk}"'}')))
+            source <(printf "${coprocSrcCode}" ${kk} ${kk} $(${nLinesAutoFlag} && printf '%s' ${kk}))
         done
        
         # wait for everything to finish
