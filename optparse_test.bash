d54c874c45100519ffe5e8c7a0485108937be009
jkool702 <107448833+jkool702@users.noreply.github.com>
2023-09-21 17:20:21 -0400

    Update optparse_test.bash

--- optparse_test.bash
+++ optparse_test.bash
@@ -1,27 +1,29 @@
+#                             Online Bash Shell.
+#                 Code, Compile, Run and Debug Bash script online.
+# Write your code in this editor and press "Run" button to execute it.
+
 #!/usr/bin/env bash
 
 gg() {
     
     local -a inAll inFun
-    local -i kk
-    local assignNextFlag assignDoneFlag flag_a var_b flag_c var_c
+    local assignNextFlag assignDoneFlag flag_a var_b flag_c var_c varAssign
     
-    assignNextFlag=false
+    assignNextFlag=''
     assignDoneFlag=false
     flag_a=false
-    flag_b=false
     flag_c=false
     
     optParse() {
         
         if ${assignDoneFlag}; then
             inFun+=("$2")
-            
-        elif ${assignNextFlag}; then 
-            varAssign="$2"
-            assignNextFlag=false
-            decalre +n varAssign
-        
+
+        elif [[ ${assignNextFlag} ]]; then 
+            declare -ng varAssign=${assignNextFlag}
+            varAssign="$2" 
+            declare +ng varAssign
+            assignNextFlag=''
         else
             case "$2" in 
                 --)  
@@ -31,24 +33,25 @@ gg() {
                     flag_a=true
                 ;;
                 -b|--bananna)
-                    declare -n varAssign=var_b
-                    assignNextFlag=true
+                    assignNextFlag=var_b
                 ;;
                 -b=|--bananna=)
                     var_b="${2#*=}"
                 ;;
                 -c|--coconut)
-                    declare -n varAssign=var_c
-                    assignNextFlag=true
+                    assignNextFlag=var_c
                     flag_c=true
                 ;;
                 -c=|--coconut=)
                     var_c="${2#*=}"
                     flag_c=true
                 ;;
-                *)
+                -*)
                     printf '\nWARNING: FLAG "%s" NOT RECOGNIZED. IGNORING.\n\n' "$2"
                 ;;
+                *)
+                    inFun+=("$2")
+                    assignDoneFlag=true
             esac
         fi
     }     
@@ -58,8 +61,8 @@ gg() {
     printf '\nSET VARIABLES:\n\n'
     printf '%s = %s\n' 'flag_a' "$flag_a" 'var_b' "$var_b" 'flag_c' "$flag_c" 'var_c' "$var_c"
     printf '\n\nNON_OPTION_INPUTS\n\n'
-    printf '"%s" ' "${inFun[@]}"
-
-}
-
-gg --apple -b 55 --coconut 'yum' 'nonOpt0' 'nonOpt1' 'nonOpt2' 'etc...'
+    printf '%q' "${inFun[@]}"
+    
+    }
+    
+    gg "$@"
