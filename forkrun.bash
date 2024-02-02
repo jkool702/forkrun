888486b45c8437b7a187a44aae1250fce6085c7a
jkool702 <107448833+jkool702@users.noreply.github.com>
2024-02-02 13:56:43 -0500

    Update forkrun.bash

--- forkrun.bash
+++ forkrun.bash
@@ -262,6 +262,13 @@ forkrun() {
 
     done
 
+    [ -t 0 ] && {
+        printf '\n\nERROR: STDIN is a terminal. \n\nforkrun requires STDIN to be a pipe \n(containing the inputs to parallelize over); e.g.: \n\nprintf '"'"'%%s\\n'"'"' "${args[@]}" | forkrun parFunc \n\nABORTING! \n\n'
+        returnVal=1
+        return 1
+    }
+    
+
     # # # # # SETUP TMPDIR # # # # #
 
     [[ ${tmpDirRoot} ]] || { [[ ${TMPDIR} ]] && [[ -d "${TMPDIR}" ]] && tmpDirRoot="${TMPDIR}"; } || { [[ -d '/dev/shm' ]] && tmpDirRoot='/dev/shm'; }  || { [[ -d '/tmp' ]] && tmpDirRoot='/tmp'; } || tmpDirRoot="$(pwd)"
