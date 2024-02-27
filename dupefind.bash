2bc837aea12e9567110aeec35a944a81ede44071
jkool702 <107448833+jkool702@users.noreply.github.com>
2024-02-27 16:20:33 -0500

    Update dupefind.bash

--- dupefind.bash
+++ dupefind.bash
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
+
 dupefind() {
-    ## quickly finds duplicate files using "forkrun", "du", and the "cksum" hash
+    ## quickly finds duplicate files using "forkrun", "du", and the "sha1sum" hash
 
 dupefind_help() ( cat<<'EOF' >&2
-## dupefind: quickly finds duplicate files using "forkrun", "du", and the "cksum" hash
+## dupefind: quickly finds duplicate files using "forkrun", "du", and the "sha1sum" hash
 #
 # dupefind implements a 2 stage search for duplicate files:
 #    it first finds files that have identical sizes, then
-#    for these files, it computes the cksum hash and looks for matching hashes
+#    for these files, it computes the sha1sum hash and looks for matching hashes
 #
 # INPUTS: are directories to look for duplicate files under (at any depth level)
 #
@@ -21,9 +22,10 @@ dupefind_help() ( cat<<'EOF' >&2
 # calling dupefind without any inputs is equivilant to calling 'dupefind / \!/{dev,proc,sys,tmp}'
 #
 # FLAGS:
-#     to skip the initial search for files with identical size, pass flag '-c' or '--cksum' or '--checksum' or '--crc' or '--hash'
+#     to skip the initial search for files with identical size, pass flag '-s' or '--sha1' or '--sha1sum' or '--sha1-only' or '--sha1sum-only'
 #     to display this help, pass flag '-h' or '-?' or '--help'
 #     to prevent printing informational info to stderr, pass flag '-q' or '--quiet'
+#     to automatically exclude '/dev' '/proc' '/run' '/sys' '/tmp', pass flag '-e' or '--exclude'
 #
 # OUTPUT: is split between stdout and stderr to allow for both easy interactive viewing and easy parsing
 #
@@ -33,32 +35,34 @@ dupefind_help() ( cat<<'EOF' >&2
 #    stderr contains the extra "fluff" to make interactive viewing of the output more pleasant
 #        redirect '2>/dev/null' or use the '-q' flag to mute this
 #
-# DEPENDENCIES: forkrun, find, du*, cksum        *not required with -c flag
+# DEPENDENCIES: forkrun, find, du*, sha1sum        *not required with -c flag
 EOF
 )
 
     set +C
+    shopt -s extglob
 
     # source forkrun
     declare -F forkrun &>/dev/null || { [[ -f ./forkrun.bash ]] && source ./forkrun.bash; } || { type -p forkrun.bash &>/dev/null && source "$(type -p forkrun.bash )"; } || source <(curl 'https://raw.githubusercontent.com/jkool702/forkrun/main/forkrun.bash') || { printf '\nERROR!!! The forkrun function is not defined and its source code could not be found/downloaded. ABORTING!\n\n'; return 1; }
 
     # make vars local
     local excludeStr fdTmpDirRoot fdTmpDir nn nnCur
-    local -a searchA excludeA useSizeFlag quietFlag dupes_size
+    local -a searchA excludeA useSizeFlag quietFlag autoExcludeFlag dupes_size
 
     # parse inputs
-    : "${useSizeFlag:=true}" "${quietFlag:=false}"
+    : "${useSizeFlag:=true}" "${quietFlag:=false}" "${autoExcludeFlag:=false}"
     if [[ $# == 0 ]]; then
         # set default runtime values
         searchA=('/')
-        excludeA=('/dev' '/proc' '/sys' '/tmp')
+        excludeA=('/dev' '/proc' '/run' '/sys' '/tmp')
     else
         # loop over inputs
         for nn in "$@"; do
             case "${nn}" in
                 -h|-\?|--help) dupefind_help; return 0 ;;
                 -q|-quiet) quietFlag=true ;;
-                -c|--cksum|--checksum|--crc|--hash) useSizeFlag=false ;;
+                -e|--exclude) autoExcludeFlag=true ;;
+                -s|--sha1|--sha1sum|--sha1-only|--sha1sum-only) useSizeFlag=false ;;
                 '!'*) excludeA+=("${nn#'!'}") ;;
                 *) searchA+=("${nn}") ;;
             esac
@@ -67,10 +71,11 @@ EOF
         [[ ${#searchA[@]} == 0 ]] && searchA=('/')
 
         # if doing a full system search 'everything under /' and not using -q flag check if they want to skip '/dev' '/proc' '/sys' '/tmp'
-        if [[ "${searchA[*]}" == '/' ]] && ! ${quietFlag}; then
-            for nnCur in '/dev' '/proc' '/sys' '/tmp'; do
-                ( IFS=' '; [[ " ${excludeA[*]} " == *' '"${nnCur}"?(/)' '* ]] ) || read -p "would you like to exclude ${nnCur} from your search for duplicate files? "$'\n'"(Y/n)  " -t 10 -n 1
-                [[ "${REPLY}" == [nN] ]] || excludeA+=("${nnCur}")
+        if [[ "${searchA[*]}" == '/' ]] && { ${autoExcludeFlag} || ! ${quietFlag}; }; then
+            for nnCur in '/dev' '/proc' '/run' '/sys' '/tmp'; do
+                ( IFS=' '; [[ " ${excludeA[*]} " == *' '"${nnCur}"?(/)' '* ]] ) || {
+                    { ${autoExcludeFlag} || { read -p "would you like to exclude ${nnCur} from your search for duplicate files? "$'\n'"(Y/n)  " -t 10 -n 1; ! [[ "${REPLY}" == [nN] ]]; }; } && excludeA+=("${nnCur}")
+                }
             done
         fi
     fi
@@ -80,7 +85,7 @@ EOF
     fdTmpDir="$(mktemp -p "${fdTmpDirRoot}" -d .dupefind.XXXXXX)"
 
     # rm tmpdir on exit
-    trap '\rm -rf "'"${fdTmpDir}"'"' EXIT
+    trap '\rm -rf "'"${fdTmpDir}"'"' EXIT INT HUP TERM
 
     ${quietFlag} || {
         printf '\n\ndupefind will now search for duplicate files under : '
@@ -90,8 +95,8 @@ EOF
             printf 'the following files/directories will be excluded: '
             printf "'%s' " "${excludeA[@]}"
         }
-        printf '\ndupefind file data will be temporairly stored under: %s\n\n' "${fdTmpDir}" >&2
-    }
+        printf '\ndupefind file data will be temporairly stored under: %s\n\n' "${fdTmpDir}" 
+    } >&2
 
     # setup find exclusions
     printf -v excludeStr ' -path '"'"'%s'"'"' -prune -o ' "${excludeA[@]}"
@@ -107,15 +112,15 @@ dupefind_size() (
     # function run in parallel by forkrun to find duplicate files size
 
     set -C
-    IFS=$'\n'
+#    IFS=$'\n'
 
     local -a fSizeA fNameA
     local -i kk
 
     # get file size/name with 'du' and split into 2 arrays
     mapfile -t fSizeA < <(du "${@}")
-    mapfile -t fNameA <<<"${fSizeA[*]#*$'\t'}"
-    mapfile -t fSizeA <<<"${fSizeA[*]%%$'\t'*}"
+    fNameA=("${fSizeA[@]#*$'\t'}")
+    fSizeA=("${fSizeA[@]%%$'\t'*}")
 
     # add each file name to a file named using the file size under ${fdTmpDir}/size/data using a '>' redirect
     # If the file already exists this will fail (due to set -C), meaning it is a duplicate. in this case append ('>>')
@@ -129,30 +134,30 @@ dupefind_size() (
 )
 
 dupefind_hash() (
-    # function run in parallel by forkrun to find duplicate file cksum hashes
+    # function run in parallel by forkrun to find duplicate file sha1sum hashes
 
     set -C
-    IFS=$'\n'
+#    IFS=$'\n'
 
     local -a fHashA fSizeA fNameA
     local -i kk
 
-    # get file hash/size/name with 'cksum' and split into 3 arrays
+    # get file size/name from name and hash with 'sha1sum' and split into 3 arrays
     fSizeA=("${@%%$'\034'*}")
     fNameA=("${@##*$'\034'}")
-    mapfile -t fHashA < <(cksum "${fNameA[@]}")
-    mapfile -t fHashA <<<"${fHashA[*]/ /_}"
-    mapfile -t fHashA <<<"${fHashA[*]%% *}"
+    mapfile -t fHashA < <(sha1sum "${fNameA[@]}" 2>&1)
+    fHashA=("${fHashA[@]%%?(sha1sum:) *}")
 
 #    for kk in ${!fHashA[@]}; do
 #        printf 'name: %s  ;  size: %s  ;  hash: %s\n' "${fNameA[$kk]}" "${fSizeA[$kk]}" "${fHashA[$kk]}" >&2
 #    done
 
-    # add each file name to a file named using the file size under ${fdTmpDir}/hash/data using a '>' redirect
+    # add each file name to a file named using the file size under ${fdTmpDir}/size/dupes/<size>/hash/data using a '>' redirect
     # If the file already exists this will fail (due to set -C), meaning it is a duplicate. in this case append ('>>')
-    # the filename instead and note that there are duplicate files with this hash by touching ${fdTmpDir}/hash/dupes/<hash>
+    # the filename instead and note that there are duplicate files with this hash by touching ${fdTmpDir}/size/dupes/<size>/hash/dupes/<hash>
      for kk in "${!fHashA[@]}"; do
-        mkdir -p "${fdTmpDir}/size/dupes/${fSizeA[$kk]}/hash"/{data,dupes}
+        [[ -z ${fHashA[$kk]} ]] && continue
+        [[ -d "${fdTmpDir}"/size/dupes/"${fSizeA[$kk]}"/hash ]] || mkdir -p "${fdTmpDir}"/size/dupes/"${fSizeA[$kk]}"/hash/{data,dupes}
         echo "${fNameA[$kk]}" >"${fdTmpDir}/size/dupes/${fSizeA[$kk]}/hash/data/${fHashA[$kk]}" || {
             echo "${fNameA[$kk]}" >>"${fdTmpDir}/size/dupes/${fSizeA[$kk]}/hash/data/${fHashA[$kk]}"
             : >"${fdTmpDir}/size/dupes/${fSizeA[$kk]}/hash/dupes/${fHashA[$kk]}"
@@ -166,7 +171,7 @@ dupefind_print() (
     for nn in "$@"; do
         nnCur="${nn//'/hash/dupes/'/'/hash/data/'}"
         printf '\n\0' >> "${nnCur}"
-            if ${quietFlag}; then
+            if ${quietFlag} || ! [ -t 1 ]; then
                 cat "${nnCur}"
             else
                 nnSize="${nn##"${fdTmpDir}/size/dupes/"}"
@@ -190,7 +195,7 @@ dupefind_print() (
             return 0
         else
             # search for duplicate file hashes by using forkrun to run dupefind_hash
-            ${quietFlag} || printf '\nBeginning search for files with identical cksum (crc hash)\n' >&2
+            ${quietFlag} || printf '\nBeginning search for files with identical sha1sum hash\n' >&2
             dupes_size=("${dupes_size[@]//'/size/dupes/'/'/size/data/'}")
             cat "${dupes_size[@]}" | forkrun dupefind_hash
         fi
@@ -205,7 +210,7 @@ dupefind_print() (
         mkdir -p "${fdTmpDir}"/hash/{data,dupes}
 
 dupefind_hash() (
-    # function run in parallel by forkrun to find duplicate file cksum hashes
+    # function run in parallel by forkrun to find duplicate file sha1sum hashes
 
     set -C
     IFS=$'\n'
@@ -213,8 +218,8 @@ dupefind_hash() (
     local -a fHashA fNameA
     local -i kk
 
-    # get file hash/name with 'cksum' and split into 2 arrays
-    mapfile -t fHashA < <(cksum "${@}")
+    # get file hash/name with 'sha1sum' and split into 2 arrays
+    mapfile -t fHashA < <(sha1sum "${@}")
     mapfile -t fHashA <<<"${fHashA[*]/ /_}"
     mapfile -t fNameA <<<"${fHashA[*]#* }"
     mapfile -t fHashA <<<"${fHashA[*]%% *}"
@@ -239,13 +244,13 @@ dupefind_print() (
             if ${quietFlag}; then
                 cat "${nnCur}"
             else
-                 printf '\n\n-------------------------------------------------------\nCKSUM HASH: %s\n\n' "${nn##*/}" 
+                 printf '\n\n-------------------------------------------------------\SHA1 HASH: %s\n\n' "${nn##*/}" 
                  cat "${nnCur}"
             fi
     done
 )
 
-        ${quietFlag} || printf '\nBeginning search for files with identical cksum (crc hash)\n' >&2
+        ${quietFlag} || printf '\nBeginning search for files with identical sha1sum hash\n' >&2
 
         # search for duplicate file hashes by using forkrun to run dupefind_hash
         source /proc/self/fd/0 <<<"find \"\${searchA[@]}\" ${excludeStr} -type f -print" | forkrun dupefind_hash
