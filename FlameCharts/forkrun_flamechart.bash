d26d9f2feca3524580b296669cb324f284e38fad
jkool702 <anthony@localhost.localdomain>
2024-07-23 11:11:36 -0400

    New FlameCharts for all the standard checksumming functions

--- FlameCharts/forkrun_flamechart.bash
+++ FlameCharts/forkrun_flamechart.bash
@@ -18,65 +18,51 @@ git clone https://github.com/brendangregg/FlameGraph.git
 . forkrun/forkrun.bash
 export -f forkrun
 
+mount -t tmpfs tmpfs /mnt/ramdisk
+
+rsync -a --max-size=$((1<<21)) /usr /mnt/ramdisk
+
 find /mnt/ramdisk -type f > /mnt/ramdisk/filelist
 
 ## info + reference times
 
 du -d 0 -h /mnt/ramdisk
-#: 39G     /mnt/ramdisk/
+#: 11G     /mnt/ramdisk/
 
 wc -l <filelist
-#: 1218957
-
-time { find /mnt/ramdisk -type f >/dev/null; }
-
-#: real    0m2.349s
-#: user    0m1.242s
-#: sys     0m1.215s
-
 
 ## confirm no dropped lines
 
 forkrun cksum <filelist | wc -l
-#: 1218957
+#: 583878
 
 forkrun sha512sum <filelist | wc -l
-#: 1218957
+#: 583878
 
 
-## cksum time
-
-time { forkrun cksum </mnt/ramdisk/filelist >/dev/null; }
-
-#: real    0m2.542s
-#: user    0m20.162s
-#: sys     0m29.681s
-
-# AVERAGE CPU UTILIZATION: 19.6 cores / 28 (logical) cores
-
-
-## sha512sum time
-
-time { forkrun sha512sum </mnt/ramdisk/filelist >/dev/null; }
-
-#: real    0m6.050s
-#: user    1m57.071s
-#: sys     0m29.004s
-
-# AVERAGE CPU UTILIZATION: 24.1 cores / 28 (logical) cores
+mapfile -t A <<<'sha1sum
+sha256sum
+sha512sum
+sha224sum
+sha384sum
+md5sum 
+sum -s
+sum -r
+cksum
+b2sum
+cksum -a sm3'
 
 
-## cksum flamechart
+for nn in "${A[@]}"; do
+    printf '\n----------------------------------------------\n%s\n\n' "$nn"
+    time { forkrun $nn </mnt/ramdisk/filelist >/dev/null; }
 
-perf record -b -g -F max /bin/bash -O extglob -c 'forkrun cksum </mnt/ramdisk/filelist >/dev/null'
-perf script > out.perf
-/mnt/ramdisk/FlameGraph/stackcollapse-perf.pl --all out.perf > out.folded
-./FlameGraph/flamegraph.pl --title="forkrun flamechart" --flamechart --hash --width 4096 --height 24 out.folded  >forkrun_cksum.svg
+    perf record -b -g -F max /bin/bash -O extglob -c 'forkrun '"$nn"' </mnt/ramdisk/filelist >/dev/null'
+    perf script > out.perf
+    /mnt/ramdisk/FlameGraph/stackcollapse-perf.pl --all out.perf > out.folded
+    ./FlameGraph/flamegraph.pl --title="forkrun flamechart -- ${nn}" --flamechart --hash --width 4096 --height 24 out.folded  >forkrun_${nn// /_}.svg
 
+    \rm -f out.* perf.data
+    sleep 1
+done
 
-## sha512sum flamechart
-
-perf record -b -g -F max /bin/bash -O extglob -c 'forkrun sha512sum </mnt/ramdisk/filelist >/dev/null'
-perf script > out.perf
-/mnt/ramdisk/FlameGraph/stackcollapse-perf.pl --all out.perf > out.folded
-./FlameGraph/flamegraph.pl --title="forkrun flamechart" --flamechart --hash --width 4096 --height 24 out.folded  >forkrun_sha512sum.svg
