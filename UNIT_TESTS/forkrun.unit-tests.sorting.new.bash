c030646a79d71f1fc9d1b301162497b35d9acc2f
jkool702 <107448833+jkool702@users.noreply.github.com>
2024-07-30 00:29:06 -0400

    Update forkrun.unit-tests.sorting.new.bash

--- UNIT_TESTS/forkrun.unit-tests.sorting.new.bash
+++ UNIT_TESTS/forkrun.unit-tests.sorting.new.bash
@@ -18,15 +18,15 @@ declare -F forkrun &>/dev/null || { [[ -f ./forkrun.bash ]] && source ./forkrun.
 which nproc 1>/dev/null 2>/dev/null && nProcs=$(nproc) || nProcs=8
 
 if ${useRamdiskFlag}; then
-	mkdir -p /mnt/ramdisk/
-	grep -qF '/mnt/ramdisk' </proc/mounts || mount -t tmpfs tmpfs /mnt/ramdisk
+    mkdir -p /mnt/ramdisk/
+    grep -qF '/mnt/ramdisk' </proc/mounts || mount -t tmpfs tmpfs /mnt/ramdisk
 
         mkdir -p /mnt/ramdisk/forkrun_unit-tests_data
-	rsync -a --max-size=$((1<<20)) "${testDir}" /mnt/ramdisk/forkrun_unit-tests_data
+    rsync -a --max-size=$((1<<20)) "${testDir}" /mnt/ramdisk/forkrun_unit-tests_data
 
-	mapfile -t -d '' A < <(find /mnt/ramdisk/forkrun_unit-tests_data -type f -print0)
+    mapfile -t -d '' A < <(find /mnt/ramdisk/forkrun_unit-tests_data -type f -print0)
 else
-	mapfile -t -d '' A < <(find "${testDir}" -type f -print0)
+    mapfile -t -d '' A < <(find "${testDir}" -type f -print0)
 fi
 
 fStr=('printf '"'"'%s\n'"'" sha1sum sha256sum)
@@ -35,22 +35,26 @@ kStr=('' '-k')
 kFix=('| sort' '')
 fFix=('' '| sed -E s/'"'"'^[0-9a-f]{40}[ \t]*'"'"'//' '| sed -E s/'"'"'^[0-9a-f]{64}[ \t]*'"'"'//')
 
-mapfile -t runArgsA < <(echo {-k\ ,}{-j\ 27\ ,}{-l\ 1\ ,}{-t\ \/tmp\ ,}{-D\ ,}{"${fStr[$fInd]}"\ ,--\ "${fStr[$fInd]}"\ ,-i\ "${fStr[$fInd]}"\ \{\}\ ,-i\ --\ "${fStr[$fInd]}"\ \{\}\ }$'\n')
-runArgsA=("${runArgsA[@]# }")
+
 
 for nArgs in $(( ${nProcs} - 2 )) $(( ${nProcs} + 2 )) $(( ${nProcs} * 128 )) $(( ${nProcs} * 1024 )); do
-	printf  '\n\n--------------------------------------------------\nBEGINNING TEST CASE FOR STDIN LENGTH = %s\n\n' "${nArgs[$nn]}"
+    printf  '\n\n--------------------------------------------------\nBEGINNING TEST CASE FOR STDIN LENGTH = %s\n\n' "${nArgs[$nn]}"
 
-	for fInd in 0 1 2; do 
-		mapfile -t -d '' C < <(printf '%s\x00' "${A[@]}" | shuf -z -n ${nArgs});
-		for runArgs in "${runArgsA[@]}"; do
+    for fInd in 0 1 2; do 
+        C=()
+        mapfile -t -d '' C < <(printf '%s\x00' "${A[@]}" | shuf -z -n ${nArgs});
 
-			[[ "${runArgs}" == '-k'* ]] && kInd=1 || kInd=0
+        runArgsA=()
+        mapfile -t runArgsA < <(echo {-k\ ,}{-j\ 27\ ,}{-l\ 1\ ,}{-t\ \/tmp\ ,}{-D\ ,}{"${fStr[$fInd]}"\ ,--\ "${fStr[$fInd]}"\ ,-i\ "${fStr[$fInd]}"\ \{\}\ ,-i\ --\ "${fStr[$fInd]}"\ \{\}\ }$'\n')
+        runArgsA=("${runArgsA[@]# }")
+        for runArgs in "${runArgsA[@]}"; do
 
-			source <(printf '%s' \(\ \{\ diff\ 2\>/dev/null\ -q\ -B\ -E\ -Z\ -d\ -a\ -b\ -w\ \<\(printf\ \'%s\\n\'\ \"\$\{C\[@\]\}\"\ \|\ forkrun\ 2\>/dev/null\ "${runArgs}"\ "${fFix[$fInd]}"\ "${kFix[$kInd]}"\ \)\ \<\(printf\ \'%s\\n\'\ \"\$\{C\[@\]\}\"\ "${kFix[$kInd]}"\)\ \&\&\ printf\ \'%s\'\ \"PASS\"\ \|\|\ printf\ \'%s\'\ \"FAIL\"\;\ printf\ \'\:\ %s\\n\'\ \'printf\ \'\"\'\"\'%s\\n\'\"\'\"\'\ \"\$\{C\[@\]\}\"\ \|\ forkrun\ 2\>/dev/null\ "${runArgs}"\ \'\;\ \}\ \|\ tee\ -a\ /tmp/.forkrun.log\;\ \)$'\n';)
+            [[ "${runArgs}" == '-k'* ]] && kInd=1 || kInd=0
 
-			source <(printf '%s' \(\ \{\ diff\ 2\>/dev/null\ -q\ -B\ -E\ -Z\ -d\ -a\ -b\ -w\ \<\(printf\ \'%s\\x00\'\ \"\$\{C\[@\]\}\"\ \|\ forkrun\ 2\>/dev/null\ -z\ "${runArgs}"\ "${fFix[$fInd]}"\ "${kFix[$kInd]}"\ \)\ \<\(printf\ \'%s\\n\'\ \"\$\{C\[@\]\}\"\ "${kFix[$kInd]}"\)\ \&\&\ printf\ \'%s\'\ \"PASS\"\ \|\|\ printf\ \'%s\'\ \"FAIL\"\;\ printf\ \'\:\ %s\\n\'\ \'printf\ \'\"\'\"\'%s\\0\'\"\'\"\'\ \"\$\{C\[@\]\}\"\ \|\ forkrun\ 2\>/dev/null\ -z\ "${runArgs}"\ \'\;\ \}\ \|\ tee\ -a\ /tmp/.forkrun.log\;\ \)$'\n';)
-		done
-	done
+            source <(printf '%s' \(\ \{\ diff\ 2\>/dev/null\ -q\ -B\ -E\ -Z\ -d\ -a\ -b\ -w\ \<\(printf\ \'%s\\n\'\ \"\$\{C\[@\]\}\"\ \|\ forkrun\ 2\>/dev/null\ "${runArgs}"\ "${fFix[$fInd]}"\ "${kFix[$kInd]}"\ \)\ \<\(printf\ \'%s\\n\'\ \"\$\{C\[@\]\}\"\ "${kFix[$kInd]}"\)\ \&\&\ printf\ \'%s\'\ \"PASS\"\ \|\|\ printf\ \'%s\'\ \"FAIL\"\;\ printf\ \'\:\ %s\\n\'\ \'printf\ \'\"\'\"\'%s\\n\'\"\'\"\'\ \"\$\{C\[@\]\}\"\ \|\ forkrun\ 2\>/dev/null\ "${runArgs@Q}"\ \'\;\ \}\ \|\ tee\ -a\ /tmp/.forkrun.log\;\ \)$'\n';)
+
+            source <(printf '%s' \(\ \{\ diff\ 2\>/dev/null\ -q\ -B\ -E\ -Z\ -d\ -a\ -b\ -w\ \<\(printf\ \'%s\\x00\'\ \"\$\{C\[@\]\}\"\ \|\ forkrun\ 2\>/dev/null\ -z\ "${runArgs}"\ "${fFix[$fInd]}"\ "${kFix[$kInd]}"\ \)\ \<\(printf\ \'%s\\n\'\ \"\$\{C\[@\]\}\"\ "${kFix[$kInd]}"\)\ \&\&\ printf\ \'%s\'\ \"PASS\"\ \|\|\ printf\ \'%s\'\ \"FAIL\"\;\ printf\ \'\:\ %s\\n\'\ \'printf\ \'\"\'\"\'%s\\0\'\"\'\"\'\ \"\$\{C\[@\]\}\"\ \|\ forkrun\ 2\>/dev/null\ -z\ "${runArgs@Q}"\ \'\;\ \}\ \|\ tee\ -a\ /tmp/.forkrun.log\;\ \)$'\n';)
+        done
+    done
 done
 
