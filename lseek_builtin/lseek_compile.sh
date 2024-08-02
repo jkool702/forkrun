e4ea2d7f72b7623c998438123030629a1f22be6a
jkool702 <107448833+jkool702@users.noreply.github.com>
2024-08-02 14:36:21 -0400

    fix typo in link

--- lseek_builtin/lseek_compile.sh
+++ lseek_builtin/lseek_compile.sh
@@ -9,7 +9,7 @@ make -j$(nproc) all
 
 cd examples/loadables
 
-curl -o ./lseek.c 'hhttps://raw.githubusercontent.com/jkool702/forkrun/main/lseek_builtin/lseek.c'
+curl -o ./lseek.c 'https://raw.githubusercontent.com/jkool702/forkrun/main/lseek_builtin/lseek.c'
 
 gcc -v -fPIC -DHAVE_CONFIG_H -DSHELL -DLOADABLE_BUILTIN -DSELECT_COMMAND -DUSING_BASH_MALLOC -g -O3 -Wno-parentheses -Wno-format-security -I. -I.. -I../.. -I../../lib -I../../builtins -I. -I../../include -I../../lib/malloc -I/mnt/ramdisk/bash -I/mnt/ramdisk/bash/lib -I/mnt/ramdisk/bash/builtins -L ../../lib/malloc -L ../.. -L /usr/lib/bash -I/usr/include/bash -I/usr/include/bash/builtins -I/usr/include/bash/include -c -o lseek.o lseek.c;
 gcc -v -DSELECT_COMMAND -DLOADABLE_BUILTIN -DHAVE_CONFIG_H -DSHELL -DUSING_BASH_MALLOC -shared -Wl,-soname,lseek -L ../../lib/malloc -L ../.. -L /usr/lib/bash -O3 -o lseek lseek.o; 
