c18d6b22ddbdc483bae407a869680cb60279d139
jkool702 <anthony@localhost.localdomain>
2024-08-02 16:56:15 -0400

    faster and more reliable unit tests

--- UNIT_TESTS/forkrun.unit-tests.sorting.new.bash
+++ UNIT_TESTS/forkrun.unit-tests.sorting.new.bash
@@ -15,7 +15,7 @@ unset forkrun
 declare -F forkrun &>/dev/null || source <(curl https://raw.githubusercontent.com/jkool702/forkrun/main/forkrun.bash)
 declare -F forkrun &>/dev/null || { [[ -f ./forkrun.bash ]] && source ./forkrun.bash; }
 
-which nproc 1>/dev/null 2>/dev/null && nProcs=$(nproc) || nProcs=8
+type nproc 1>/dev/null 2>/dev/null && nProcs=$(nproc) || nProcs=8
 
 mkdir -p /mnt/ramdisk/forkrun_unit-tests_data
 
@@ -25,9 +25,9 @@ if ${useRamdiskFlag}; then
 
     rsync -a --max-size=$((1<<20)) "${testDir}" /mnt/ramdisk/forkrun_unit-tests_data
 
-    mapfile -t -d '' A < <(find /mnt/ramdisk/forkrun_unit-tests_data -type f -print0)
+    find /mnt/ramdisk/forkrun_unit-tests_data -type f -print0 > /mnt/ramdisk/forkrun_unit-tests_data/filelist0
 else
-    mapfile -t -d '' A < <(find "${testDir}" -type f -print0)
+    find "${testDir}" -type f -print0 > /mnt/ramdisk/forkrun_unit-tests_data/filelist0
 fi
 
 fStr=('printf '"'"'%s\n'"'" sha1sum sha256sum)
@@ -38,12 +38,12 @@ fFix=('' '| sed -E s/'"'"'^[0-9a-f]{40}[ \t]*'"'"'//' '| sed -E s/'"'"'^[0-9a-f]
 
 
 for nArgs in $(( ${nProcs} - 2 )) $(( ${nProcs} + 2 )) $(( ${nProcs} * 128 )) $(( ${nProcs} * 1024 )); do
-    printf  '\n\n--------------------------------------------------\nBEGINNING TEST CASE FOR STDIN LENGTH = %s\n\n' "${nArgs[$nn]}"
+    printf  '\n\n--------------------------------------------------\nBEGINNING TEST CASE FOR STDIN LENGTH = %s\n\n' "${nArgs}"
 
     for fInd in 0 1 2; do 
        
-        printf '%s\n' "${A[@]}" | shuf -n ${nArgs} >/mnt/ramdisk/forkrun_unit-tests_data/fileListCur
-        printf '%s\x00' "${A[@]}" | shuf -z -n ${nArgs} >/mnt/ramdisk/forkrun_unit-tests_data/fileListCur0
+        shuf -z -n ${nArgs} </mnt/ramdisk/forkrun_unit-tests_data/filelist0 | tr '\0' '\n'  >/mnt/ramdisk/forkrun_unit-tests_data/filelistCur
+        shuf -z -n ${nArgs} </mnt/ramdisk/forkrun_unit-tests_data/filelist0  >/mnt/ramdisk/forkrun_unit-tests_data/filelistCur0
 
         runArgsA=()
         mapfile -t runArgsA < <(echo {-k\ ,}{-j\ 27\ ,}{-l\ 1\ ,}{-t\ \/tmp\ ,}{-D\ ,}{"${fStr[$fInd]}"\ ,--\ "${fStr[$fInd]}"\ ,-i\ "${fStr[$fInd]}"\ \{\}\ ,-i\ --\ "${fStr[$fInd]}"\ \{\}\ }$'\n')
@@ -52,9 +52,9 @@ for nArgs in $(( ${nProcs} - 2 )) $(( ${nProcs} + 2 )) $(( ${nProcs} * 128 )) $(
 
             [[ "${runArgs}" == '-k'* ]] && kInd=1 || kInd=0
 
-            source <(printf '%s' \(\ \{\ diff\ 2\>/dev/null\ -q\ -B\ -E\ -Z\ -d\ -a\ -b\ -w\ \<\(forkrun\ \<'/mnt/ramdisk/forkrun_unit-tests_data/fileListCur'\ 2\>/dev/null\ "${runArgs}"\ "${fFix[$fInd]}"\ "${kFix[$kInd]}"\ \)\ \<\(printf\ \'%s\\n\'\ \"\$\{C\[@\]\}\"\ "${kFix[$kInd]}"\)\ \&\&\ printf\ \'%s\'\ \"PASS\"\ \|\|\ printf\ \'%s\'\ \"FAIL\"\;\ printf\ \'\:\ %s\\n\'\ \'printf\ \'\"\'\"\'%s\\n\'\"\'\"\'\ \"\$\{C\[@\]\}\"\ \|\ forkrun\ 2\>/dev/null\ "${runArgs//%s/%s\\}"\ \'\;\ \}\ \|\ tee\ -a\ /tmp/.forkrun.log\;\ \)$'\n';)
+	    source /proc/self/fd/0 <<<\(\ \{\ diff\ 2\>/dev/null\ -q\ -B\ -E\ -Z\ -d\ -a\ -b\ -w\ \<\(forkrun\ \</mnt/ramdisk/forkrun_unit-tests_data/filelistCur\ 2\>/dev/null\ "${runArgs}"\ "${fFix[$fInd]}"\ "${kFix[$kInd]}"\ \)\ \<\(cat\ /mnt/ramdisk/forkrun_unit-tests_data/filelistCur\ "${kFix[$kInd]}"\)\ \&\&\ printf\ \'%s\'\ \"PASS\"\ \|\|\ printf\ \'%s\'\ \"FAIL\"\;\ printf\ \'\:\ %s\\n\'\ \'\ forkrun\ 2\>/dev/null\ "${runArgs//%s/%s\\}"\ \'\;\ \}\ \|\ tee\ -a\ /tmp/.forkrun.log\;\ \)$'\n'
 
-            source <(printf '%s' \(\ \{\ diff\ 2\>/dev/null\ -q\ -B\ -E\ -Z\ -d\ -a\ -b\ -w\ \<\(forkrun\ \<'/mnt/ramdisk/forkrun_unit-tests_data/fileListCur0'\ 2\>/dev/null\ -z\ "${runArgs}"\ "${fFix[$fInd]}"\ "${kFix[$kInd]}"\ \)\ \<\(printf\ \'%s\\n\'\ \"\$\{C\[@\]\}\"\ "${kFix[$kInd]}"\)\ \&\&\ printf\ \'%s\'\ \"PASS\"\ \|\|\ printf\ \'%s\'\ \"FAIL\"\;\ printf\ \'\:\ %s\\n\'\ \'printf\ \'\"\'\"\'%s\\0\'\"\'\"\'\ \"\$\{C\[@\]\}\"\ \|\ forkrun\ 2\>/dev/null\ -z\ "${runArgs//%s/%s\\}"\ \'\;\ \}\ \|\ tee\ -a\ /tmp/.forkrun.log\;\ \)$'\n';)
+	    source /proc/self/fd/0 <<<\(\ \{\ diff\ 2\>/dev/null\ -q\ -B\ -E\ -Z\ -d\ -a\ -b\ -w\ \<\(forkrun\ \</mnt/ramdisk/forkrun_unit-tests_data/filelistCur0\ 2\>/dev/null\ -z\ "${runArgs}"\ "${fFix[$fInd]}"\ "${kFix[$kInd]}"\ \)\ \<\(tr\ "'"\\0"'"\ "'"\\n"'"\ \</mnt/ramdisk/forkrun_unit-tests_data/filelistCur0\ "${kFix[$kInd]}"\)\ \&\&\ printf\ \'%s\'\ \"PASS\"\ \|\|\ printf\ \'%s\'\ \"FAIL\"\;\ printf\ \'\:\ %s\\n\'\ \'\ forkrun\ 2\>/dev/null\ -z\ "${runArgs//%s/%s\\}"\ \'\;\ \}\ \|\ tee\ -a\ /tmp/.forkrun.log\;\ \)$'\n'
         done
     done
 done
