f683c2abe8b48adbea2143195091128797e90190
jkool702 <107448833+jkool702@users.noreply.github.com>
2025-07-24 02:02:19 -0400

    Update forkrun_flamechart.bash

--- FlameCharts/forkrun_flamechart.bash
+++ FlameCharts/forkrun_flamechart.bash
@@ -12,17 +12,23 @@
 
 cd /mnt/ramdisk
 
-git clone https://github.com/jkool702/forkrun.git
+#git clone https://github.com/jkool702/forkrun.git
 git clone https://github.com/brendangregg/FlameGraph.git
 
-. forkrun/forkrun.bash
+#. forkrun/forkrun.bash
 export -f forkrun
 
-mount -t tmpfs tmpfs /mnt/ramdisk
+renice --priority -20 --pid $$
 
-rsync -a --max-size=$((1<<21)) /usr /mnt/ramdisk
+[[ "$USER" == 'root' ]] && {
+	for nn in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do echo 'performance' >"${nn}"; done
+}
 
-find /mnt/ramdisk -type f > /mnt/ramdisk/filelist
+#mount -t tmpfs tmpfs /mnt/ramdisk
+
+#rsync -a --max-size=$((1<<21)) /usr /mnt/ramdisk
+
+find /mnt/ramdisk -type f -print0> /mnt/ramdisk/filelist0
 
 ## info + reference times
 
@@ -33,10 +39,10 @@ wc -l <filelist
 
 ## confirm no dropped lines
 
-forkrun cksum <filelist | wc -l
+forkrun -z cksum <filelist0 | wc -l
 #: 583878
 
-forkrun sha512sum <filelist | wc -l
+forkrun -z sha512sum <filelist0 | wc -l
 #: 583878
 
 
@@ -55,12 +61,44 @@ cksum -a sm3
 xxhsum
 xxhsum -H3'
 
+ff() {
+sha1sum "${@}"
+sha256sum "${@}"
+sha512sum "${@}"
+sha224sum "${@}"
+sha384sum "${@}"
+sha512sum "${@}"
+md5sum  "${@}"
+sum -s "${@}"
+sum -r "${@}"
+cksum "${@}"
+b2sum "${@}"
+cksum -a sm3 "${@}"
+xxhsum "${@}"
+xxhsum -H3 "${@}"
+}
+export -f ff
 export A
 
-  perf record -b -g -F max /bin/bash -O extglob -c 'for nn in "${A[@]}"; do forkrun '"$nn"' </mnt/ramdisk/filelist >/dev/null; done'
+
+mkdir -p ./test1 ./test2
+
+cd ./test1
+  perf record -b -g -F max /bin/bash -O extglob -c 'forkrun -z ff </mnt/ramdisk/filelist >/dev/null'
     perf script > out.perf
     /mnt/ramdisk/FlameGraph/stackcollapse-perf.pl --all out.perf > out.folded
-    ./FlameGraph/flamegraph.pl --title="forkrun flamechart -- ALL" --flamechart --hash --width 4096 --height 24 out.folded  >forkrun_combined_new.svg
+    /mnt/ramdisk/FlameGraph/flamegraph.pl --title="forkrun -- 13 checksums (combined via ff)" --flamechart --width 4096 --height 24 out.folded  >forkrun_all_combined-ff_new.svg
+    /mnt/ramdisk/timep/timep_flamegraph.pl --title="forkrun --color=time -- 13 checksums (combined via ff)" --flamechart --width 4096 --height 24 out.folded  >forkrun_all_combined-ff_time.svg
+
+cd ../test2
+  perf record -b -g -F max /bin/bash -O extglob -c 'for nn in "${A[@]}"; do forkrun -z '"$nn"' </mnt/ramdisk/filelist0 >/dev/null; done'
+    perf script > out.perf
+    /mnt/ramdisk/FlameGraph/stackcollapse-perf.pl --all out.perf > out.folded
+    /mnt/ramdisk/FlameGraph/flamegraph.pl --title="forkrun -- 13 checksums (seperate)" --flamechart --hash --width 4096 --height 24 out.folded  >forkrun_all_new.svg
+    /mnt/ramdisk/timep/timep_flamegraph.pl --title="forkrun --color=time -- 13 checksums" --flamechart --width 4096 --height 24 out.folded  >forkrun_all_combined-ff_time.svg
+    
+  cd ../
+
 
 
 for nn in "${A[@]}"; do
