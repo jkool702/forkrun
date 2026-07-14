e9f635bb73d92fc1ddafb42eb2c75c7532c88888
jkool702 <BuddhaEnergy13542>
2026-07-13 22:43:47 -0400

    HOTFIX: bump to v3.4.1

--- frun.bash
+++ frun.bash
@@ -615,7 +615,7 @@ EOF
             # help system
             -h|-\?|--help|--help=*|--usage)  _frun_displayHelp "$1";  return 0  ;;
 
-            -V|--version|--VERSION)           echo 'forkrun v3.4.0';  return 0  ;;
+            -V|--version|--VERSION)           echo 'forkrun v3.4.1';  return 0  ;;
 
             --) shift; break ;;
 
