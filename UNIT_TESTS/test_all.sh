cab5ec126ae3ef8fd0d2821f46fe74b8c8aad28d
jkool702 <BuddhaEnergy13542>
2026-08-28 14:05:51 -0400

    update test all unit test script

--- UNIT_TESTS/test_all.sh
+++ UNIT_TESTS/test_all.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/bash
+
+cat /sys/kernel/mm/transparent_hugepage/shmem_enabled | grep -F '[always]' >/dev/null || { echo 'enabling THP' >&2; echo always | sudo tee /sys/kernel/mm/transparent_hugepage/shmem_enabled; }
+
 ./test_c_plugins.sh
 ./test_c_plugins_v2.sh
 ./test_frun.sh
