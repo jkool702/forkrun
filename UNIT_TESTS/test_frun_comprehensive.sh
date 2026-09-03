5f7970629ab4df4a58dfcd60205813e73ef80fd2
jkool702 <BuddhaEnergy13542>
2026-09-03 19:24:20 -0400

    update unit test

--- UNIT_TESTS/test_frun_comprehensive.sh
+++ UNIT_TESTS/test_frun_comprehensive.sh
@@ -3565,7 +3565,7 @@ if in_section R; then
     _MD="$TEST_DIR/R_SLURM_TERM"; mkdir -p "$_MD"
     seq 10000 > "$_MD/input.txt"; rm -f "$_MD/.forkrun_resume"
 
-    bash -c "source '$FRUN_SOURCE'; cd '$_MD'; cat input.txt | FORKRUN_PREEMPT_MODE=1 frun -k -s -l 1 sleep 0.1 & FPID=\$!; sleep 0.3; CPID=\$(ps -o pid= --ppid \$FPID 2>/dev/null | tr -d ' '); [[ -n \"\$CPID\" ]] && (( CPID = CPID + 7 )) && kill -TERM \$CPID 2>/dev/null || kill -TERM \$FPID 2>/dev/null; wait \$FPID" \
+    bash -c "source '$FRUN_SOURCE'; cd '$_MD'; cat input.txt | FORKRUN_PREEMPT_MODE=1 frun -k -s -l 1 sleep 0.1 & FPID=\$!; sleep 0.3; CPID=\$(ps -o pid= --ppid \$FPID 2>/dev/null | tr -d ' '); [[ -n \"\$CPID\" ]] && (( CPID = CPID + 6 )) && kill -TERM \$CPID 2>/dev/null || kill -TERM \$FPID 2>/dev/null; wait \$FPID" \
         > "$_MD/output.txt" 2>"$_MD/err.txt"
     _REXIT=$?
 
