e4ab7c146995ec40d0dbbf8e55ef1ed15a7c2a9a
jkool702 <BuddhaEnergy13542>
2026-09-19 12:55:17 -0400

    remove -c from frun exec

--- frun.bash
+++ frun.bash
@@ -155,7 +155,7 @@ frun() {
 
         # EXEC into Clean Room
         # /proc/self/fd/ is safer than $BASHPID in some namespace contexts
-        exec -c "${BASH:-bash}" --norc --noprofile -c 'enable -f "/proc/self/fd/'"${FORKRUN_MEMFD_LOADABLES}"'" '"${ring_enable}"' ring_list
+        exec "${BASH:-bash}" --norc --noprofile -c 'enable -f "/proc/self/fd/'"${FORKRUN_MEMFD_LOADABLES}"'" '"${ring_enable}"' ring_list
 export LC_ALL=C
 export _FR_IN_CLEANROOM=1
 set +m
